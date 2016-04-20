d4f3002d50f22318a215be45c5b801354ad35cbb
Ariel Tian <astian@andrew.cmu.edu>
2016-04-19 20:13:28 -0400

    Create board

--- board.h
+++ board.h
@@ -4,12 +4,12 @@ struct board
 {
   int dim;
 
-  // cells = dim x dim array of ints, each is a bit vector of possible values
+  // cells = dim x dim array of ints, each: is a bit vector of possible values
   // i.e. 010000000 = 8 is the value of the cell
   // i.e. 010011010 = 2, 4, 5, 8 are possible values of cell
   int** cells;
+
+
+
+
 };
-
-typedef struct board* Board;
-
-/* Board functions here */
