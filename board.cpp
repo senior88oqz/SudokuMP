480b373c4818a26ae3d3d30e829bb5c769177767
Ariel Tian <astian@andrew.cmu.edu>
2016-04-30 18:49:38 -0400

    Add some easier helper functions

--- board.cpp
+++ board.cpp
@@ -8,6 +8,9 @@
 
 enum Align {ROW, COL, BLOCK};
 
+/************ Helper functions **************************************/
+
+/* Turn i, j, align into row, col */
 void index_to_row_col(int i, int j, Align align, int &row, int &col) {
   if (align == ROW) {
     row = i;
@@ -16,10 +19,81 @@ void index_to_row_col(int i, int j, Align align, int &row, int &col) {
     row = j;
     col = i;
   } else {
-    row = (i / board->inner_dim) + (j / board->inner_dim);
-    col = (i % board->inner_dim) + (j % board->inner_dim);
+    row = (i/board->inner_dim)*board->inner_dim + (j/board->inner_dim);
+    col = (i%board->inner_dim)*board->inner_dim + (j%board->inner_dim);
   }
 }
+
+/* Return true if old_changed is all zeroes */
+bool nothing_has_changed() {
+  for(int r = 0; r < board->dim; r++) {
+    for(int c = 0; c < board->dim; c++) {
+      if (board->old_changed[r][c])
+        return 1;
+    }
+  }
+  return 0;
+}
+
+/* Swap old_changed and new_changed, clear new_changed */
+void clear_changed() {
+  bool** temp = board->new_changed;
+  board->new_changed = board->old_changed;
+  board->old_changed = temp;
+
+  for(int r = 0; r < board->dim; r++) {
+    for(int c = 0; c < board->dim; c++) {
+      board->new_changed = 0;
+    }
+  }
+}
+
+/* Remove all instances of value from row/col/block i */
+void clear_number(int i, Align align, int value) {
+  int mask = ~ (1 << (value-1));
+  if(align == ROW) {
+    for(int c = 0; c < board->dim; c++)
+      board->cells[i][c] = mask & board->cells[i][c];
+  } else if (align == COL) {
+    for(int r = 0; r < board->dim; r++)
+      board->cells[r][i] = mask & board->cells[r][i];
+  } else {
+    int id = board->inner_dim;
+    for(int r = (i/id) * id; r < (i/id)*id + id; r++) {
+      for(int c = (i%id) * id; c < (i%id)*id + id; c++) {
+        board->cells[r][c] = mask & board->cells[r][c];
+      }
+    }
+  }
+}
+
+
+/* Test is elimination is possible (i.e. 010000000 -> 8)
+ * If possible, update solution and remove num from row, col, and block
+ */
+bool elimination(int i, int j, Align align) {
+  int row, col, value, num = 1;
+  index_to_row_col(i, j, align, row, col);
+
+  if (!board->solution[row][col]) {
+    value = board->cells[row][col];
+    while(value % 2 == 0) {
+      value = value/2;
+      num++;
+    }
+    if(value == 1) {
+      int id = board->inner_dim;
+      board->solution[row][col] = num;
+      clear_number(row, ROW, num);
+      clear_number(col, COL, num);
+      clear_number((row/id)*id + (col/id), BLOCK, num);
+    }
+  }
+
+}
+
+/********************************************************************/
+
 
 void print_board(){
     std::string line = "";
@@ -77,6 +151,8 @@ bool create_board(const char* filename, int dim) {
         bits = 1 << (num-1);
       board->solution[row][col] = num;
       board->cells[row][col] = bits;
+      board->old_changed[row][col] = 1;
+      board->new_changed[row][col] = 0;
     }
   }
   return 1;
